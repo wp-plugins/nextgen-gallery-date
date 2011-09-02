caef6e422a2998c8a07c2108dc793277b45f6bc3
roberto.c <roberto.c@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-02 08:07:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/nextgen-gallery-date/tags/0.1.5@432108 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- date/date.php
+++ date/date.php
@@ -1,34 +1,23 @@
 <?php
 if (is_admin()){	
 
-	// FUNC: save creation date, create custom post id and post meta on gallery creation _________________________________________________________________________________________________
+	// FUNC: save creation date __________________________________________________________________________________________________
 	
 		function rcwd_ngg_gallery_date_insert($gid){
 			global $wpdb, $nggdb;
 			$wpdb->query("UPDATE ".$wpdb->prefix."ngg_gallery SET added_date = now() WHERE gid = ".$gid);	
-			$gallery = $nggdb->find_gallery($gid);
-			$post_id = wp_insert_post(array( 'post_type' => 'rcwd-ngg', 'post_title' => $gallery->title, 'post_author' => $gallery->author, 'post_status' => 'publish', 'comment_status' => 'open' ));
-			update_post_meta($post_id, '_rcwd_nggid', $gid);
 		}
 		add_action('ngg_created_new_gallery', 'rcwd_ngg_gallery_date_insert');
 
-	// FUNC: save modification date, check custom post id and post meta on gallery update ______________________________________________________________________________________________
+	// FUNC: save modification date _______________________________________________________________________________________________
 	
 		function rcwd_ngg_gallery_date_update($gid, $post){
 			global $wpdb, $nggdb;
 			$wpdb->query("UPDATE ".$wpdb->prefix."ngg_gallery SET modified_date = now() WHERE gid = ".$gid);
-			$post_id	= rcwd_get_postid_from_custom('_rcwd_nggid', $gid);
-			$the_post 	= false;
-			if($post_id !== false) $the_post = get_post($post_id);
-			if( $post_id === false or !$the_post or ( $the_post->post_type != 'rcwd-ngg')){
-				$gallery = $nggdb->find_gallery($gid);
-				$post_id = wp_insert_post(array('post_type' => 'rcwd-ngg', 'post_title' => $gallery->title, 'post_author' => $gallery->author, 'post_status' => 'publish', 'comment_status' => 'open' ));
-				update_post_meta($post_id, '_rcwd_nggid', $gid);	
-			}
 		}
 		add_action('ngg_update_gallery', 'rcwd_ngg_gallery_date_update', 10, 2);
 
-	// FUNC: save modification date on image add ______________________________________________________________________________________________
+	// FUNC: save modification date on image add __________________________________________________________________________________
 	
 		function rcwd_ngg_date_update_after_new_images_added($galleryID, $image_ids){
 			global $wpdb;
@@ -38,7 +27,7 @@ if (is_admin()){
 		
 }else{	
 
-	// FUNC: order by creation date _________________________________________________________________________________________________
+	// FUNC: order by creation date _______________________________________________________________________________________________
 
 		function rcwd_ngg_gallery_order($galleries, $album){
 			global $rcwdNggDate;
@@ -75,6 +64,7 @@ if (is_admin()){
 // FUNC: add date info to gallery object ______________________________________________________________________________________________
 
 	function rcwd_add_date_to_gallery_object($gallery, $galleryID){
+		echo 'ZZZZZZZ';
 		global $wpdb;
 		$gallery_info 					= $wpdb->get_row('SELECT * FROM '.$wpdb->nggallery.' WHERE gid = '.$galleryID);
 		$picturesCounter 				= $wpdb->get_row('SELECT COUNT(*) as counter FROM '.$wpdb->nggpictures.' WHERE galleryid = '.$galleryID);
@@ -87,9 +77,9 @@ if (is_admin()){
 		$gallery->modified_time			= $gallery_info->modified_date;
 		$gallery->since_modified_date	= $gallery_info->modified_date;
 		if(!is_null($gallery_info->modified_date) and $gallery_info->modified_date != ''){
-			$gallery->modified_date 		= date_i18n( get_option('time_format'), strtotime($gallery_info->modified_date) );
+			$gallery->modified_date 		= date_i18n( get_option('date_format'), strtotime($gallery_info->modified_date) );
 			$gallery->modified_time			= date_i18n( get_option('time_format'), strtotime($gallery_info->modified_date) );	
-			$gallery->since_modified_date	= rcwd_nggdate_since($gallery->modified_date);
+			$gallery->since_modified_date	= rcwd_nggdate_since($gallery->sql_modified_date);
 		}
 		$gallery->author 				= $gallery_info->author;
 		$gallery->counter 				= $picturesCounter->counter;
